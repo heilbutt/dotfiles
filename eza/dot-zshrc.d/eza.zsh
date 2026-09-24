70d1eda934728fbd4a332664f7e324e0059e0a54
Hermann Pommerenke <dev@pommerenke.ch>
2026-09-24 16:41:26 +0200

    Modular zshrc

---
+++ eza/dot-zshrc.d/eza.zsh
@@ -0,0 +1,2 @@
+alias ls="eza --group-directories-first --color=auto"
+alias ll="eza -lga --group-directories-first --color=auto"
---
+++ miniforge/dot-zshrc.d/miniforge.zsh
@@ -0,0 +1,5 @@
+# File content is based on what is automatically created by the conda init command
+# It is needed so the conda and what is installed by it are available in the shell
+if [[ -x /opt/homebrew/Caskroom/miniforge/base/bin/conda ]]; then
+  eval "$(/opt/homebrew/Caskroom/miniforge/base/bin/conda shell.zsh hook)"
+fi
