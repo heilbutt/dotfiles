a5e860eca59d6117dd0df12ea4eda3c59d724ae6
Hermann Pommerenke <dev@pommerenke.ch>
2026-09-24 16:44:43 +0200

    More eza aliases for mac/zsh

--- eza/dot-zshrc.d/eza.zsh
+++ eza/dot-zshrc.d/eza.zsh
@@ -1,2 +1,4 @@
-alias ls="eza --group-directories-first --color=auto"
-alias ll="eza -lga --group-directories-first --color=auto"
+alias ls='eza --group-directories-first --icons'
+alias ll='eza -la --git --group-directories-first --icons'
+alias llm='eza -la --git --icons --sort=modified'
+alias lt='eza -a --tree -L2 --group-directories-first --icons'
